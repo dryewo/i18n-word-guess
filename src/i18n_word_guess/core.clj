3350cbbea78407f030b92812769555146d65703e
Dmitry Balakhonskiy <dnomaid@gmail.com>
2014-03-08 17:42:16 +0400

    Disable caching

--- src/i18n_word_guess/core.clj
+++ src/i18n_word_guess/core.clj
@@ -9,27 +9,34 @@
 
 (defmodel Guess {:word String})
 
+(defn wrap-cache-control [handler]
+  (fn [request]
+    (let [resp (handler request)
+          headers (:headers resp)]
+      (assoc-in resp [:headers "Cache-control"] "no-cache"))))
+
 (defapi app
   compojure.api.middleware/public-resource-routes
   (swagger-ui "/api")
   (swagger-docs)
-  (swaggered "i18n-word-guess"
-             :description "Слово угадай игра"
-             (context "/games" []
-                      (GET* "/new" []
-                            :summary  "Создать новую игру"
-                            (ok (run/new-game)))
-                      (GET* "/all" []
-                            :summary  "Получить статусы всех игр"
-                            (ok (run/get-all-games))))
-             (context "/game/:game_id" [game_id]
-                      (GET* "/" []
-                            :summary  "Получить статус игры по ID"
-                            (ok (run/get-game game_id)))
-                      (GET* "/guess" [word]
-                            :summary  "Попробовать угадать"
-                            :query [guess Guess]
-                            (ok (run/guess-game game_id word))))))
+  (with-middleware [wrap-cache-control]
+    (swaggered "i18n-word-guess"
+               :description "Слово угадай игра"
+               (context "/games" []
+                        (GET* "/new" []
+                              :summary  "Создать новую игру"
+                              (ok (run/new-game)))
+                        (GET* "/all" []
+                              :summary  "Получить статусы всех игр"
+                              (ok (run/get-all-games))))
+               (context "/game/:game_id" [game_id]
+                        (GET* "/" []
+                              :summary  "Получить статус игры по ID"
+                              (ok (run/get-game game_id)))
+                        (GET* "/guess" [word]
+                              :summary  "Попробовать угадать"
+                              :query [guess Guess]
+                              (ok (run/guess-game game_id word)))))))
 
 (defn -main [& args]
   (let [port (or (first args) 3030)]
